37d48b566042087734872ac52f503d8008c2ce81
Paulius Ruikis <paulius_ruikis@cargill-fms.com>
2022-03-14 21:28:14 +0200

    implememting visual state management, night and day time styles for ananlog clock

--- CustomControls/AnalogClock.cs
+++ CustomControls/AnalogClock.cs
@@ -72,8 +72,22 @@ namespace AnalogClock.CustomControls
         protected virtual void OnTimeChanged(DateTime time)
         {
             UpdateHandAngles(time);
+            UpdateTimeState(time);
             RaiseEvent( new RoutedPropertyChangedEventArgs<DateTime>(DateTime.Now.AddSeconds(-1),DateTime.Now, TimeChangedEvent ));
         }
+
+        private void UpdateTimeState(DateTime time)
+        {
+            if (time.Hour >6 && time.Hour<18)
+            {
+                VisualStateManager.GoToState(this, "Day", false);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Night", false);
+            }
+        }
+
         private void UpdateHandAngles(DateTime time)
         {
             hourHand.RenderTransform = new RotateTransform((time.Hour / 12.0) * 360, 0.5, 0.5);
